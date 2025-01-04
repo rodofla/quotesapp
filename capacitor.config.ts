8be5fc07951c6f900624ff5e533d5a11855d6f85
Rodrigo Flores <rodfloresp@gmail.com>
2025-01-04 17:27:34 -0300

    initialize new Ionic Angular application with basic structure and configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.quotesapp',
+  appName: 'quotesapp',
+  webDir: 'www'
+};
+
+export default config;
